c821e8b92edeca55ae40bfa34795a51190eab231
Matheus Nicolini <matheus.cunha@task.com.br>
2021-07-07 01:27:33 -0300

    Criando layout das informações do usuário no dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,12 +1,34 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React from 'react';
 
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  Icon
+} from './styles';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/49494932?v=4'}} />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Matheus</UserName>
+            </User>
+          </UserInfo>
+
+          <Icon name='power' />
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
